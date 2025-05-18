fe654ee6c922394895d2edf8d8e1e2ea7b3221b2
AamodJain <2023csb1092@iitrpr.ac.in>
2025-05-19 03:21:55 +0530

    added data schemas

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from .user_model import User
+from .course_model import Course
+from .week_model import Week
+from .subtopic_model import Subtopic
+from .subtopic_content_model import Subtopic_content
